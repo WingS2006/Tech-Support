54ad91ae6af3084f521798ba0c05a13187e0622f
Wing Sze <jessicakwok20060609@gmail.com>
2026-03-24 03:10:14 -0400

    Did exercice 4

--- Responder.java
+++ Responder.java
@@ -1,4 +1,5 @@
-
+import java.util.Random;
+import java.util.ArrayList;
 /**
  * The responder class represents a response generator object.
  * It is used to generate an automatic response to an input string.
@@ -8,20 +9,34 @@
  */
 public class Responder
 {
+    private Random generator;
+    private ArrayList<String> responses;
+    
     /**
      * Construct a Responder - nothing to do
      */
     public Responder()
     {
+        generator = new Random();
+        responses = new ArrayList<>();
+        fillResponses();
     }
 
+    public void fillResponses()
+    {
+        responses.add("Please read the manual.");
+        responses.add("Please call an expert.");
+        responses.add("Please reboot.");
+        responses.add("That sounds interesting. Tell me more...");
+    }
+    
     /**
      * Generate a response.
      * @return   A string that should be displayed as the response
      */
     public String generateResponse()
     {
-        return "That sounds interesting. Tell me more...";
+        return responses.get(generator.nextInt(responses.size()));
     }
 }
 
