abb1d24e933c73d4907f1f0767776b1ca52ebf19
Wing Sze <jessicakwok20060609@gmail.com>
2026-03-24 02:47:25 -0400

    Exercise 2

--- SupportSystem.java
+++ SupportSystem.java
@@ -40,7 +40,7 @@ public class SupportSystem
         while(!finished) {
             String input = reader.getInput();
 
-            if(input.trim().startsWith("bye")) {
+            if(input.trim().toLowerCase().startsWith("bye")) {
                 finished = true;
             }
             else {
